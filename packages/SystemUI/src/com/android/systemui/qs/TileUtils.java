073c723de95f108e22abb4d998aa21b55c6bf385
minaripenguin <minaripenguin@users.noreply.github.com>
2023-04-03 08:01:15 +0800

    QSPanel: Increase label text size a step
    * Users complained about it being too small
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/TileUtils.java
+++ packages/SystemUI/src/com/android/systemui/qs/TileUtils.java
@@ -102,7 +102,7 @@ public class TileUtils {
     public static float getQSTileLabelSize(Context context) {
         return (float) Settings.System.getIntForUser(context.getContentResolver(),
                 Settings.System.QS_TILE_LABEL_SIZE,
-                14, UserHandle.USER_CURRENT);
+                15, UserHandle.USER_CURRENT);
     }
 
     public static float getQSTileSecondaryLabelSize(Context context) {
